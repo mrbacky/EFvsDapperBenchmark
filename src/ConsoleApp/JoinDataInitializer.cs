a637080a067090e6d21545b098b0e2b5c6030fc8
mrbacky <radoslav.backovsky@gmail.com>
2022-06-08 17:31:09 +0200

    issues with running

--- src/ConsoleApp/JoinDataInitializer.cs
+++ src/ConsoleApp/JoinDataInitializer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bogus;
@@ -10,14 +11,39 @@ namespace ConsoleApp;
 
 public class JoinDataInitializer
 {
-    private ApplicationDbContext _context;
-    private int GetRandomId() => new Random().Next(1, 10);
+    private readonly ApplicationDbContext _context;
+    private readonly List<int> _randomNumbers;
+
+    // private int GetRandomId() => new Random().Next(1, 10);
+    private int counter;
+
     public JoinDataInitializer(ApplicationDbContext context)
     {
         _context = context;
+        _randomNumbers = GenerateRandomNumbers(1, 10000, 10000);
     }
-    public  async Task Init()
+
+    private int GetRandomId()
     {
+        var x = counter;
+        counter++;
+        // Console.WriteLine("In GetRandomId > x: " + x);
+        // Console.WriteLine("In GetRandomId > list:  " + randomNumbers);
+        return _randomNumbers[x];
+    }
+
+    public List<int> GenerateRandomNumbers(int from, int to, int numberOfElement)
+    {
+        var random = new Random();
+        var numbers = new HashSet<int>();
+        while (numbers.Count <= numberOfElement) numbers.Add(random.Next(from, to));
+        return numbers.ToList();
+    }
+
+    public async Task Init()
+    {
+        Console.WriteLine("IN  INIT");
+
         await InitStudents();
         await InitCourses();
         await InitCourseStudent();
@@ -25,12 +51,12 @@ public class JoinDataInitializer
 
     public async Task InitStudents()
     {
+        Console.WriteLine("IN Student INIT");
         var studentFaker = new Faker<Student>("tr")
             .RuleFor(i => i.FirstName, i => i.Person.FirstName)
             .RuleFor(i => i.LastName, i => i.Person.LastName)
             .RuleFor(i => i.BirthDate, i => i.Person.DateOfBirth);
-
-        var students = studentFaker.Generate(100);
+        var students = studentFaker.Generate(10000);
 
         await _context.AddRangeAsync(students);
         await _context.SaveChangesAsync();
@@ -38,30 +64,26 @@ public class JoinDataInitializer
 
     public async Task InitCourses()
     {
-
-
         var courseFaker = new Faker<Course>("tr")
-        .RuleFor(i => i.Name, i => i.Commerce.Department());
-        var courses = courseFaker.Generate(10);
-        var students = _context.Students.ToListAsync();
-
+            .RuleFor(i => i.Name, i => i.Commerce.Department());
+        var courses = courseFaker.Generate(2000);
         await _context.Courses.AddRangeAsync(courses);
         await _context.SaveChangesAsync();
     }
 
     public async Task InitCourseStudent()
     {
-        string sql = @"INSERT INTO course_student (CoursesId, StudentsId)
-                                VALUES ({0}, {1})";
-        for (int studentId = 1; studentId <= 20; studentId++)
-        {
+        Console.WriteLine("LIST: " + _randomNumbers.Count);
 
-            for (int courseId = 1; courseId <= 5 ; courseId++)
-            {
-                Console.WriteLine("courseId: " + courseId + " *  studentId: " + studentId);
-                await _context.Database.ExecuteSqlRawAsync(sql, courseId, studentId);
-            }
+        var sql = @"INSERT INTO course_student (CoursesId, StudentsId)
+                                VALUES ({0}, {1})";
+        for (var studentId = 1; studentId <= 1000; studentId++)
+        {
+            var courseId = GetRandomId();
+            Console.WriteLine("courseId: " + courseId + " *  studentId: " + studentId);
+            await _context.Database.ExecuteSqlRawAsync(sql, GetRandomId(), studentId);
         }
+
         await _context.SaveChangesAsync();
     }
 }
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,67 +1,60 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Running;
 using ConsoleApp.Persistence.Dapper.Mapping;
 using ConsoleApp.Persistence.EF.Context;
 using ConsoleApp.Tests;
+using Dapper;
 using Dapper.FluentMap;
 using Dapper.FluentMap.Dommel;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading.Tasks;
-using Bogus;
-using ConsoleApp.Domain.Entities;
-using Microsoft.Data.SqlClient;
 
-namespace ConsoleApp
+namespace ConsoleApp;
+
+public static class Program
 {
-    public static class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var dbContextOptions = InitEf();
+        var context = new ApplicationDbContext(dbContextOptions);
+        context.Database.EnsureCreatedAsync();
+        Console.WriteLine("In Main");
+        var joinDataInitializer = new JoinDataInitializer(context);
+        joinDataInitializer.Init();
+
+        // context.Database.Migrate();
+
+        // InitDapper();
+
+        // BenchmarkRunner.Run<InsertTest>();
+        // BenchmarkRunner.Run<InsertManyTest>();
+        BenchmarkRunner.Run<SelectJoinTest>();
+        // BenchmarkRunner.Run<SelectTest>();
+        // BenchmarkRunner.Run<SearchTest>();
+        // BenchmarkRunner.Run<FunctionsTest>();
+        // BenchmarkRunner.Run<UpdateTest>();
+        // BenchmarkRunner.Run<DeleteTest>();
+
+
+        Console.ReadLine();
+    }
+
+    public static void InitDapper()
+    {
+        DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+        FluentMapper.Initialize(config =>
         {
+            config.AddMap(new StudentMap());
+            config.ForDommel();
+        });
+    }
 
-            // var dbContextOptions = InitEf();
-            // var context = new ApplicationDbContext(dbContextOptions);
-            // context.Database.EnsureCreated();
+    public static DbContextOptions InitEf()
+    {
+        var builder = new DbContextOptionsBuilder();
+        builder.UseSqlServer(Constants.ConnectionStringEF);
+        builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 
-            // var joinDataInitializer = new JoinDataInitializer(context);
-            // await joinDataInitializer.Init();
-
-            // context.Database.Migrate();
-
-            // InitDapper();
-
-            BenchmarkRunner.Run<InsertTest>();
-            // BenchmarkRunner.Run<InsertManyTest>();
-            // BenchmarkRunner.Run<SelectJoinTest>();
-            BenchmarkRunner.Run<SelectTest>();
-            // BenchmarkRunner.Run<SearchTest>();
-            // BenchmarkRunner.Run<FunctionsTest>();
-            // BenchmarkRunner.Run<UpdateTest>();
-            // BenchmarkRunner.Run<DeleteTest>();
-
-
-            Console.ReadLine();
-        }
-
-        public static void InitDapper()
-        {
-            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-
-            FluentMapper.Initialize(config =>
-            {
-                config.AddMap(new StudentMap());
-                config.ForDommel();
-            });
-        }
-
-        public static DbContextOptions InitEf()
-        {
-            var builder = new DbContextOptionsBuilder();
-            builder.UseSqlServer(Constants.ConnectionStringEF);
-            builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-
-            return builder.Options;
-        }
+        return builder.Options;
     }
 }
